d095133f6f25bd8d5c65cf7d551739845597c4c6
Uladzislau Klimianok <v1rus3731@gmail.com>
2026-08-01 15:49:58 +0200

    Enhance UserRepository: Update findByEmployee_EmployeeNumber query to use LEFT JOIN FETCH for eager loading of employee and position to prevent LazyInitializationException.

--- src/main/java/com/warehouse/demo/repository/user/UserRepository.java
+++ src/main/java/com/warehouse/demo/repository/user/UserRepository.java
@@ -3,10 +3,11 @@ package com.warehouse.demo.repository.user;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.warehouse.demo.entity.user.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    // SELECT u FROM User u LEFT JOIN u.employee e WHERE e.employeeNumber = :employeeNumber
+    @Query("SELECT u FROM User u LEFT JOIN FETCH u.employee e LEFT JOIN FETCH e.position WHERE e.employeeNumber = :employeeNumber") // evade LazyInitializationException
     Optional<User> findByEmployee_EmployeeNumber(String employeeNumber); 
 }
